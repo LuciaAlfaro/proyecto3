9e38e04abfb52b0b273f30c9b08a1f7f8d7df5d9
AndresEsparzaALU <3481343@alu.murciaeduca.es>
2022-11-22 09:26:41 +0100

    Layout y primera vista

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('encuentro', function() {
+Route::get('encuentro', function () {
     return view('encuentro');
+});
+Route::get('contact', function () {
+    return view('contact');
 });
