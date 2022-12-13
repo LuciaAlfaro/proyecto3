4bbb05a806aba8a5fc14638f573167793245b0a3
AndresEsparzaALU <3481343@alu.murciaeduca.es>
2022-12-13 21:05:56 +0100

    Ejercicios controladores y formularios //todos los apartados

--- resources/views/productos/create.blade.php
+++ resources/views/productos/create.blade.php
@@ -1,4 +1,43 @@
 @extends('layouts.master')
 @section('content')
     <h1>Pantalla create</h1>
+
+    <div class="row" style="margin-top:40px">
+        <div class="offset-md-3 col-md-6">
+            <div class="card">
+                <div class="card-header text-center">
+                    Añadir Vehículo
+                </div>
+                <div class="card-body" style="padding:30px">
+
+                    <form action="{{ url('/catalog/create') }}" method="POST">
+
+                        @csrf
+
+                        <div class="form-group">
+                            <label for="title">Tipo vehículo</label>
+                            <input type="text" name="tipo" id="tipo" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="title">Descripción</label>
+                            <input type="text" name="descripcion" id="descripcion" class="form-control">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="title">Imagen</label>
+                            <input type="text" name="imagen" id="imagen" class="form-control">
+                        </div>
+
+                        <div class="form-group text-center">
+                            <button type="submit" class="btn btn-primary" style="padding:8px 100px;margin-top:25px;">
+                                Añadir vehículo
+                            </button>
+                        </div>
+
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    </div>
 @stop
--- resources/views/productos/edit.blade.php
+++ resources/views/productos/edit.blade.php
@@ -1,4 +1,43 @@
 @extends('layouts.master')
 @section('content')
     <h1>Pantalla edit del producto {{$id}}</h1>
+
+    <div class="row" style="margin-top:40px">
+        <div class="offset-md-3 col-md-6">
+            <div class="card">
+                <div class="card-header text-center">
+                    Modificar vehículo
+                </div>
+                <div class="card-body" style="padding:30px">
+
+                    <form action="{{ url('/catalog/create') }}" method="POST">
+                        {{method_field('PUT')}}
+                        @csrf
+
+                        <div class="form-group">
+                            <label for="title">Tipo vehículo</label>
+                            <input type="text" name="tipo" id="tipo" class="form-control" value="{{$bici['tipo']}}">
+                        </div>
+                        <div class="form-group">
+                            <label for="title">Descripción</label>
+                            <input type="text" name="descripcion" id="descripcion" class="form-control" value="{{$bici['descripcion']}}">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="title">Imagen</label>
+                            <input type="text" name="imagen" id="imagen" class="form-control" value="{{$bici['imagen']}}">
+                        </div>
+
+                        <div class="form-group text-center">
+                            <button type="submit" class="btn btn-primary" style="padding:8px 100px;margin-top:25px;">
+                                Modificar vehículo
+                            </button>
+                        </div>
+
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    </div>
 @stop
