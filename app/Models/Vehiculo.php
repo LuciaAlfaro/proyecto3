bebfe534754e9b1c9c93ad298504c123dada5509
Lucia Alfaro <1535381@alu.murciaeduca.es>
2023-02-21 11:13:51 +0100

    Tabla customer_vehiculo

--- app/Models/Vehiculo.php
+++ app/Models/Vehiculo.php
@@ -18,14 +18,25 @@ class Vehiculo extends Model
     ];
 
     /**
-     * Devuelve el tipoVehiculo asociado a un order.
+     * Devuelve el tipoVehiculo asociado a un vehiculo.
      */
     public function tipoVehiculo()
     {
         return $this->belongsTo(TipoVehiculo::class, 'tipoVehiculo_id');
     }
+    /**
+     * Devuelve la station asociada a un vehiculo.
+     */
     public function station()
     {
         return $this->belongsTo(Station::class, 'station_id');
+    }
+
+    /**
+     * Los customer que tiene asignados un determinado vehiculo.
+     */
+    public function customers()
+    {
+        return $this->belongsToMany(Customer::class, 'vehiculo_customer', 'vehiculo_id', 'customer_id');
     }
 }
---
+++ database/migrations/2023_02_21_075441_create_customer_vehiculo_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomerVehiculoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customer_vehiculo', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('customer_id');
+            $table->unsignedBigInteger('vehiculo_id');
+            $table->timestamps();
+            $table->foreign('customer_id')->references('id')->on('customers');
+            $table->foreign('vehiculo_id')->references('id')->on('vehiculos');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customer_vehiculo');
+    }
+}
